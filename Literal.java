4bc12d725061dd06a04cbe6d7177aac3f5ba8bf9
anirudhreddy9966 <139291438+anirudhreddy9966@users.noreply.github.com>
2024-04-02 04:05:32 -0500

    Add files via upload

---
+++ Literal.java
@@ -0,0 +1,12 @@
+
+import java.lang.*;
+
+public class Literal
+{
+   public static void main(String args[])
+   {
+    byte b=15;
+    System.out.println(b);
+
+   }
+}
---
+++ Literals.java
@@ -0,0 +1,20 @@
+
+import java.lang.*;
+
+public class Literals
+{
+   public static void main(String args[])
+   {
+    byte b1=10;
+    byte b2=0b1010;
+    byte b3=012;
+    byte b4=0XA;
+    long l=9999999L;
+   System.out.println(l);
+    System.out.println(b1);
+    System.out.println(b2);
+    System.out.println(b3);
+    System.out.println(b4);
+
+   }
+}
